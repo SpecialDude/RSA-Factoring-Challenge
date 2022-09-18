2f99fd5ffbf21f9a01534639895eb33b1366b1e4
SpecialDude <warithadetayo.awa@gmail.com>
2022-09-18 23:37:24 +0100

    Factors

--- 0-factor.c
+++ 0-factor.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "BigInt.h"
 
 /**
  * open_file - Open up a file for reading
@@ -74,7 +75,7 @@ int main(int ac, char **av)
     char *filename, *eptr;
     FILE *fd;
     char *line;
-    unsigned long long number, i, j;
+    unsigned long long number, i, j, k, l, mid, third;
 
     if (ac != 2)
         exit(EXIT_FAILURE);
@@ -88,11 +89,29 @@ int main(int ac, char **av)
 
         number = strtoull(line, &eptr, 10);
 
-        i = 2;
-        j = number / 2;
+		if (number % 2 == 0)
+		{
+			printf("%llu=%llu*%llu\n", number, number/2, 2);
+			free(line);
+			continue;
+		}
 
-        while (i < number / 2 && j > 2)
+
+		mid = number / 2;
+		third = 3 * number / 4;
+
+		mid = (mid % 2 == 1) ? mid : mid + 1;
+		third = (third % 2 == 1) ? third : third + 1;
+
+        i = 3;			// Increasing
+		j = mid; // Decreasing
+		k = mid + 2;		// Increasing   3 * number / 4
+		l = number - 2;
+
+
+        while ((i <= (mid/2) + 1 && j > (mid/2) + 1) || (k <= third && l > third))
         {
+			// printf("(%llu	%llu) <----> (%llu	%llu)\n", i, j, k, l);
             if (number % i == 0)
             {
                 printf("%llu=%llu*%llu\n", number, number/i, i);
@@ -104,8 +123,23 @@ int main(int ac, char **av)
                 printf("%llu=%llu*%llu\n", number, j, number/j);
                 break;
             }
-            i++;
-            j--;
+
+			if (number % k == 0)
+            {
+                printf("%llu=%llu*%llu\n", number, number/k, k);
+                break;
+            }
+
+            if (number % l == 0)
+            {
+                printf("%llu=%llu*%llu\n", number, l, number/l);
+                break;
+            }
+
+            i += 2;
+            j -= 2;
+			k += 2;
+			l -= 2;
         }
 
 		free(line);
